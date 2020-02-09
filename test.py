5671e66425cefe176adac558a1e08787f70fb067
Trogious <trog@swmud.net>
2020-02-09 15:00:12 +0000

    test fix 2

--- test.py
+++ test.py
@@ -38,12 +38,9 @@ if __name__ == '__main__':
         print('fork failed: %d' % pid)
         sys.exit(1)
     elif pid == 0:
-        d = TestDaemon()
-        d.start()
-        d = TestDaemon2()
-        d.start()
-        d = TestDaemon3()
-        d.start()
+        daemnons = [TestDaemon(), TestDaemon2(), TestDaemon3()]
+        for d in daemnons:
+            d.start()
     else:
         time.sleep(1)
         logfile = './aio_daemon.log'
